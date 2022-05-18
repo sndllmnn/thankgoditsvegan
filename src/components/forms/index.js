45a5375fc39c82b5f892dc5c461f5d31b9077f63
Sina <user@MacBook-Air-de-User.local>
2022-05-18 15:43:04 +0200

    start folder structure

---
+++ src/components/forms/index.js
@@ -0,0 +1,8 @@
+/* import { TextField } from './TextField/TextField'
+import { Select } from './Select/Select'
+import { Radio } from './Radio/Radio'
+
+export { TextField, Select, Radio }
+Then when you need to use one or more of the components, you can easily import them all at once.
+
+import { TextField, Select, Radio } from '@components/forms' */
